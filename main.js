e60ba85b4c73ac47f6db7de0a79b6148148a11d1
Zhengning Li <zl132@georgetown.edu>
2015-10-10 10:43:26 -0400

    new version

--- main.js
+++ main.js
@@ -1,52 +1,108 @@
-// Initialize Phaser, and create a 400x490px game
 var game = new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv');
 
-// Create our 'main' state that will contain the game
 var mainState = {
 
     preload: function() { 
-        // Change the background color of the game
         game.stage.backgroundColor = '#71c5cf';
 
-        // Load the bird sprite
-        game.load.image('bird', 'assets/bird.png');
+        game.load.image('bird', 'assets/bird.png');  
+        game.load.image('pipe', 'assets/pipe.png'); 
+
+        // Load the jump sound
+        game.load.audio('jump', 'assets/jump.wav');     
     },
 
     create: function() { 
-        // Set the physics system
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        // Display the bird on the screen
+        this.pipes = game.add.group();
+        this.pipes.enableBody = true;
+        this.pipes.createMultiple(20, 'pipe');  
+        this.timer = this.game.time.events.loop(1500, this.addRowOfPipes, this);           
+
         this.bird = this.game.add.sprite(100, 245, 'bird');
-
-        // Add gravity to the bird to make it fall
         game.physics.arcade.enable(this.bird);
-        this.bird.body.gravity.y = 1000;  
+        this.bird.body.gravity.y = 1000; 
 
-        // Call the 'jump' function when the spacekey is hit
+        // New anchor position
+        this.bird.anchor.setTo(-0.2, 0.5); 
+ 
         var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        spaceKey.onDown.add(this.jump, this);     
+        spaceKey.onDown.add(this.jump, this); 
+
+        this.score = 0;
+        this.labelScore = this.game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });  
+
+        // Add the jump sound
+        this.jumpSound = this.game.add.audio('jump');
     },
 
     update: function() {
-       // If the bird is out of the world (too high or too low), call the 'restartGame' function
         if (this.bird.inWorld == false)
-            this.restartGame();
+            this.restartGame(); 
+
+        game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this); 
+
+        // Slowly rotate the bird downward, up to a certain point.
+        if (this.bird.angle < 20)
+            this.bird.angle += 1;     
     },
 
-    // Make the bird jump 
-    jump: function() {  
-        // Add a vertical velocity to the bird
+    jump: function() {
+        // If the bird is dead, he can't jump
+        if (this.bird.alive == false)
+            return; 
+
         this.bird.body.velocity.y = -350;
+
+        // Jump animation
+        game.add.tween(this.bird).to({angle: -20}, 100).start();
+
+        // Play sound
+        this.jumpSound.play();
     },
 
-    // Restart the game
-    restartGame: function() {  
-        // Start the 'main' state, which restarts the game
+    hitPipe: function() {
+        // If the bird has already hit a pipe, we have nothing to do
+        if (this.bird.alive == false)
+            return;
+            
+        // Set the alive property of the bird to false
+        this.bird.alive = false;
+
+        // Prevent new pipes from appearing
+        this.game.time.events.remove(this.timer);
+    
+        // Go through all the pipes, and stop their movement
+        this.pipes.forEachAlive(function(p){
+            p.body.velocity.x = 0;
+        }, this);
+    },
+
+    restartGame: function() {
         game.state.start('main');
+    },
+
+    addOnePipe: function(x, y) {
+        var pipe = this.pipes.getFirstDead();
+
+        pipe.reset(x, y);
+        pipe.body.velocity.x = -200;  
+        pipe.checkWorldBounds = true;
+        pipe.outOfBoundsKill = true;
+    },
+
+    addRowOfPipes: function() {
+        var hole = Math.floor(Math.random()*5)+1;
+        
+        for (var i = 0; i < 8; i++)
+            if (i != hole && i != hole +1) 
+                this.addOnePipe(400, i*60+10);   
+    
+        this.score += 1;
+        this.labelScore.text = this.score;  
     },
 };
 
-// Add and start the 'main' state to start the game
 game.state.add('main', mainState);  
 game.state.start('main'); 
