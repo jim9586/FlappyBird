691aef1d2eb3542cba249396ed65670fa9dad0f4
Zhengning Li <zl132@georgetown.edu>
2015-10-10 11:31:12 -0400

    new version

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var game = new Phaser.Game(800, 800, Phaser.AUTO, 'gameDiv');
+var game = new Phaser.Game(400, 1200, Phaser.AUTO, 'gameDiv');
 
 var mainState = {
 
     preload: function() { 
-        //game.stage.backgroundColor = '#71c5cf';
+        game.stage.backgroundColor = '#71c5cf';
 
         game.load.image('bird', 'assets/bird.png');  
         game.load.image('pipe', 'assets/pipe.png'); 
